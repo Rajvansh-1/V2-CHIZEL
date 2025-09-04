c4d19ccf77b45765872c363548e86e4ca21ef293
Rajvansh-1 <rajvansh2525@gmail.com>
2025-09-04 23:41:23 +0530

    fixed issue footer

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -29,7 +29,7 @@ const HomePage = () => {
   return (
     <>
       <Navbar />
-      <Suspense fallback={<div></div>}>
+      <Suspense fallback={<div style={{ height: '100vh' }}></div>}>
         <HeroSection />
         <ProblemStatementSection />
         <SolutionSection />
