1b2c687cf8026beb48df7d8b4848e7240ef89c15
Rajvansh-1 <rajvansh2525@gmail.com>
2025-09-04 22:22:54 +0530

    push before production

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,26 +1,36 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { trackPageview } from "@/utils/analytics";
+
 import MainLayout from "@components/layout/MainLayout";
 import HomePage from "@/pages/home";
 import ChizelWebPage from "@/pages/chizel-web";
 
+// Define routes
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
-      {
-        index: true, 
-        element: <HomePage />,
-      },
-      {
-        path: "chizel-web", 
-        element: <ChizelWebPage />,
-      },
+      { index: true, element: <HomePage /> },
+      { path: "chizel-web", element: <ChizelWebPage /> },
     ],
   },
 ]);
 
+// Router wrapper with Google Analytics
 const AppRouter = () => {
+  useEffect(() => {
+    // Subscribe to route changes
+    const unsubscribe = router.subscribe(({ location }) => {
+      trackPageview(location.pathname + location.search);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return <RouterProvider router={router} />;
 };
 
