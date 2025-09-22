e7535a848b39f45f5b2345f06f001192a1ab2193
Rajvansh-1 <rajvansh2525@gmail.com>
2025-09-23 01:10:17 +0530

    updated lenis

--- src/hooks/useLenisScroll.js
+++ src/hooks/useLenisScroll.js
@@ -1,14 +1,25 @@
 // src/hooks/useLenisScroll.js
 import { useEffect } from 'react';
 import Lenis from '@studio-freight/lenis';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const useLenisScroll = () => {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.0, // <-- EXPERIMENT WITH THIS VALUE (e.g., 0.8, 1.0, 1.5)
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // easeOutExpo
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothTouch: true,
     });
+
+    // Sync GSAP's ScrollTrigger with Lenis
+    lenis.on('scroll', ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
 
     let rafId;
     function raf(time) {
