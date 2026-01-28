7561cc509ae6d6d643cfebe38adcf764b5713036
Rajvansh-1 <rajvansh2525@gmail.com>
2026-01-28 18:43:12 +0530

    Update Lenis smoothTouch setting to false and adjust CSS for overflow handling

--- src/hooks/useLenisScroll.js
+++ src/hooks/useLenisScroll.js
@@ -9,7 +9,7 @@ const useLenisScroll = () => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothTouch: true,
+      smoothTouch: false,
     });
 
     // Sync GSAP's ScrollTrigger with Lenis
